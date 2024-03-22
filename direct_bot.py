be6c75e3897b9bd700d60db87afb5535eec78d6a
Muhammad Adhitya <95895044+adhit007@users.noreply.github.com>
2024-03-22 13:30:26 +0700

    Update program, must be running on direct message menu

--- direct_bot.py
+++ direct_bot.py
@@ -1,70 +1,38 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import time, random
+import time
 
-# Username and password of your instagram account:
-my_username = '' # Enter Your Username Instagram
-my_password = '' # Enter Your Password Instagram
+options = webdriver.ChromeOptions()
+options.add_experimental_option("debuggerAddress", "localhost:9222")
+browser = webdriver.Chrome(options=options)
 
-# Instagram username list for DM:
-usernames = ['',''] # Enter username target send message
+# Enter username/keyword target send message
+usernames = ['', ''] 
 
 # Messages:
-messages = ['example 1','example 2', 'example 3'] # Enter Your Message Here
-
-# Delay time between messages in sec:
-between_messages = 5
-
-browser = webdriver.Chrome('chromedriver')
-
-# Authorization:
-def auth(username, password):
-	try:
-		browser.get('https://instagram.com')
-		time.sleep(random.randrange(2,4))
-
-		input_username = browser.find_element_by_name('username')
-		input_password = browser.find_element_by_name('password')
-
-		input_username.send_keys(username)
-		time.sleep(random.randrange(1,2))
-		input_password.send_keys(password)
-		time.sleep(random.randrange(1,2))
-		input_password.send_keys(Keys.ENTER)
-
-	except Exception as err:
-		print(err)
-		browser.quit()
-
-# Sending messages:
-def send_message(users, messages):
-	try:
-		browser.find_element_by_xpath('/html/body/div[1]/section/nav/div[2]/div/div/div[3]/div/div[2]/a').click()
-		time.sleep(random.randrange(3,5))
-		browser.find_element_by_xpath('/html/body/div[6]/div/div/div/div[3]/button[2]').click()
-		time.sleep(random.randrange(1,2))
-		browser.find_element_by_xpath('/html/body/div[1]/section/div/div[2]/div/div/div[2]/div/div[3]/div/button').click()
-		for user in users:
-			time.sleep(random.randrange(1,2))
-			browser.find_element_by_xpath('/html/body/div[6]/div/div/div[2]/div[1]/div/div[2]/input').send_keys(user)
-			time.sleep(random.randrange(2,3))
-			browser.find_element_by_xpath('/html/body/div[6]/div/div/div[2]/div[2]').find_element_by_tag_name('button').click()
-			time.sleep(random.randrange(3,4))
-			browser.find_element_by_xpath('/html/body/div[6]/div/div/div[1]/div/div[2]/div/button').click()
-			time.sleep(random.randrange(3,4))
-			text_area = browser.find_element_by_xpath('/html/body/div[1]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[2]/textarea')
-			text_area.send_keys(random.choice(messages))
-			time.sleep(random.randrange(2,4))
-			text_area.send_keys(Keys.ENTER)
-			print(f'Message successfully sent to {user}')
-			time.sleep(between_messages)
-			browser.find_element_by_xpath('/html/body/div[1]/section/div/div[2]/div/div/div[1]/div[1]/div/div[3]/button').click()
-
-	except Exception as err:
-		print(err)
-		browser.quit()
+messages = ['hello world! :)'] # Enter Your Message Here
 
 
-auth(my_username, my_password)
-time.sleep(random.randrange(2,4))
-send_message(usernames, messages)
+for user in usernames:
+        try:
+                    time.sleep(10)
+                    browser.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[1]/div/div/div/div/div/div[5]/div/div/div/span/div/a/div').click()
+                    browser.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[2]/section/div/div/div/div[1]/div/div[2]/div/div/div/div[4]/div').click()
+                    time.sleep(3)
+                    input_target = browser.find_element(By.XPATH, '/html/body/div[6]/div[1]/div/div[2]/div/div/div/div/div/div/div[1]/div/div[2]/div/div[2]/input')
+                    input_target.send_keys(user)
+                    time.sleep(5)
+                    browser.find_element(By.XPATH, '/html/body/div[6]/div[1]/div/div[2]/div/div/div/div/div/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div').click()
+                    time.sleep(3)
+                    browser.find_element(By.XPATH, '/html/body/div[6]/div[1]/div/div[2]/div/div/div/div/div/div/div[1]/div/div[4]/div').click()
+                    browser.implicitly_wait(10)
+                    time.sleep(10)
+                    input_message = browser.find_element(By.XPATH, '/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[2]/section/div/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div[2]/div/div/div[2]/div/div/div[2]/div/div[1]')
+                    input_message.send_keys(messages)
+                    time.sleep(3)
+                    input_message.send_keys(Keys.ENTER)         
+        except Exception as err:
+                print(err)
+        else:
+                print(f'Message successfully sent to {usernames}')
